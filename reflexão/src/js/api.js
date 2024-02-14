2d454097b2caf414d8050f5590e6aafc7dbe22b7
Zhawrt <153181055+ZhawrtDev@users.noreply.github.com>
2024-02-14 13:08:15 -0300

    Reflexão quase pronto
    so falta a responsividade e o model do mobile

---
+++ reflexão/src/js/api.js
@@ -0,0 +1,157 @@
+// BANNER
+let nextPageToken = "";
+const banner = document.querySelector(".img-banner");
+const tituloReflexao = document.querySelector(".text-banner");
+
+function getBanner() {
+  fetch(
+    "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCJ1RTO5MjmLlgI5rlEEqCiA&maxResults=3&order=date&key=AIzaSyC2QsGmSVOXlkJaLjyT3vdNcEClBv0Oyes&pageToken=" +
+      nextPageToken
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      banner.innerHTML = `
+            <a href="https://www.youtube.com/watch?v=${data.items[0].id.videoId}" target="_blank">
+                <img src="${data.items[0].snippet.thumbnails.medium.url}">
+            </a>
+        `;
+
+      tituloReflexao.innerHTML = `
+            <h1>${data.items[0].snippet.title}</h1>
+            <h2>${data.items[0].snippet.publishedAt}</h2>
+            <p>${data.items[0].snippet.description}</p>
+            <a href="https://www.youtube.com/watch?v=${data.items[0].id.videoId}" target="_blank">
+               Assistir <i class="uil uil-play"></i>
+            </a>
+        `;
+    });
+}
+getBanner();
+
+// RECENTES
+function getVideo() {
+  fetch(
+    "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCJ1RTO5MjmLlgI5rlEEqCiA&maxResults=3&order=date&key=AIzaSyC2QsGmSVOXlkJaLjyT3vdNcEClBv0Oyes&pageToken=" +
+      nextPageToken
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      nextPageToken = data.nextPageToken;
+      let videos = data.items;
+      let ultimo = document.querySelector(".tds");
+      ultimo.innerHTML = "";
+      for (video of videos) {
+        ultimo.innerHTML += `
+          <div class='box video'>
+          <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank">
+          <img class='hinolink img' src="${video.snippet.thumbnails.medium.url}" />
+          </a>
+          </div>
+        `;
+      }
+    });
+}
+getVideo();
+
+// DESTAQUE
+const channelId = "UCJ1RTO5MjmLlgI5rlEEqCiA";
+const apiKey = "AIzaSyC2QsGmSVOXlkJaLjyT3vdNcEClBv0Oyes";
+
+const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=viewCount&maxResults=3`;
+
+function loadVideos() {
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      let videos = data.items;
+      let videosContainer = document.getElementById("videos");
+      videosContainer.innerHTML = "";
+
+      videos.forEach((video) => {
+        const videoId = video.id.videoId;
+        const videoTitle = video.snippet.title;
+        const videoThumbnail = video.snippet.thumbnails.medium.url;
+
+        videosContainer.innerHTML += `
+                  <div class="box video">
+                      <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
+                          <img class='hinolink img' src="${videoThumbnail}" alt="${videoTitle}">
+                      </a>
+                  </div>
+              `;
+      });
+    })
+    .catch((error) => {
+      console.error("Erro ao carregar os vídeos:", error);
+    });
+}
+
+loadVideos();
+
+//RECOMENDADOS
+const RecomendadoKey = "AIzaSyC2QsGmSVOXlkJaLjyT3vdNcEClBv0Oyes";
+let recomendadoUrl = `https://www.googleapis.com/youtube/v3/search?key=${RecomendadoKey}&channelId=${channelId}&part=snippet,id&order=rating&maxResults=3`;
+
+function RecomendadosFunc() {
+  fetch(recomendadoUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      let videos = data.items;
+      let videosContainer = document.getElementById("Recomandados");
+      videosContainer.innerHTML = "";
+
+      videos.forEach((video) => {
+        const videoId = video.id.videoId;
+        const videoTitle = video.snippet.title;
+        const videoThumbnail = video.snippet.thumbnails.medium.url;
+
+        videosContainer.innerHTML += `
+        <div class="box video">
+          <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
+              <img class='hinolink img' src="${videoThumbnail}" alt="${videoTitle}">
+           </a>
+        </div>
+              `;
+      });
+    })
+    .catch((error) => {
+      console.error("Erro ao carregar os vídeos:", error);
+    });
+}
+
+RecomendadosFunc();
+
+// SEARCH
+const videoContainer = document.querySelector("#pop-up-input-main");
+
+function searchYourVideos() {
+  const searchQuery = searchInput.value;
+  const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyC8CaMGmTdfrBNNVYlOHWnaEWydYK8imYQ&channelId=UCJ1RTO5MjmLlgI5rlEEqCiA&q=${searchQuery}&part=snippet&type=video&maxResults=19`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      data.items.forEach((item) => {
+        const videoId = item.id.videoId;
+        const videoTitle = item.snippet.title;
+        const videoThumbnail = item.snippet.thumbnails.medium.url;
+
+        const videoElement = `
+                            <div class="box-search video">
+                                <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
+                                    <img class='hinolink img' src="${videoThumbnail}" alt="${videoTitle}">
+                                </a>
+                            </div>
+                        `;
+
+        videoContainer.innerHTML += videoElement;
+      });
+    })
+    .catch((error) => {
+      console.error("Error searching for videos:", error);
+    });
+}
--- reflexão/src/js/main.js
+++ reflexão/src/js/main.js
@@ -1,131 +1,66 @@
 // Navigation Script
 
-// BANNER
-let nextPageToken = "";
-const banner = document.querySelector(".img-banner");
-const tituloReflexao = document.querySelector(".text-banner");
+// Darck Theme
+var isColorChanged = false;
 
-function getBanner() {
-  fetch(
-    "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCJ1RTO5MjmLlgI5rlEEqCiA&maxResults=3&order=date&key=AIzaSyAdnNB5zv3AItIx260W4XrDfbT-PZmZKzs&pageToken=" +
-      nextPageToken
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      banner.innerHTML = `
-            <a href="https://www.youtube.com/watch?v=${data.items[0].id.videoId}" target="_blank">
-                <img src="${data.items[0].snippet.thumbnails.medium.url}">
-            </a>
-        `;
+var buttons = document.querySelectorAll(".uil-moon");
 
-      tituloReflexao.innerHTML = `
-            <h1>${data.items[0].snippet.title}</h1>
-            <h2>${data.items[0].snippet.publishedAt}</h2>
-            <p>${data.items[0].snippet.description}</p>
-            <a href="https://www.youtube.com/watch?v=${data.items[0].id.videoId}" target="_blank">
-               Assistir <i class="uil uil-play"></i>
-            </a>
-        `;
-    });
-}
-getBanner();
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    if (isColorChanged) {
+      document.documentElement.style.setProperty("--color-black", "#342A24");
+      document.documentElement.style.setProperty("--color-gray", "#616063");
+      document.documentElement.style.setProperty("--color-fundo", "#8e7a62");
+      document.documentElement.style.setProperty("--background-color", "#fff");
+      document.documentElement.style.setProperty(
+        "--color-destaques",
+        "#897c6e"
+      );
+      document.documentElement.style.setProperty("--color-destaques", "#fff");
+      document.documentElement.style.setProperty(
+        "--shadow-destaque",
+        "1px 1px 12px #333"
+      );
+    } else {
+      document.documentElement.style.setProperty("--color-black", "#D3C4CB");
+      document.documentElement.style.setProperty("--color-gray", "#D1CED8");
+      document.documentElement.style.setProperty(
+        "--background-color",
+        "#1e1e1e"
+      );
+      document.documentElement.style.setProperty(
+        "--color-destaques",
+        "#161616"
+      );
+      // document.documentElement.style.setProperty("--color-fundo", "#5C4D3E");
+      document.documentElement.style.setProperty(
+        "--shadow-destaque",
+        "1px 1px 12px rgba(255,255,255 0.100)"
+      );
+    }
+    isColorChanged = !isColorChanged;
+  });
+});
 
-// RECENTES
-function getVideo() {
-  fetch(
-    "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCJ1RTO5MjmLlgI5rlEEqCiA&maxResults=3&order=date&key=AIzaSyAdnNB5zv3AItIx260W4XrDfbT-PZmZKzs&pageToken=" +
-      nextPageToken
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      nextPageToken = data.nextPageToken;
-      let videos = data.items;
-      let ultimo = document.querySelector(".tds");
-      ultimo.innerHTML = "";
-      for (video of videos) {
-        ultimo.innerHTML += `
-          <div class='box video'>
-          <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank">
-          <img class='hinolink img' src="${video.snippet.thumbnails.medium.url}" />
-          </a>
-          </div>
-        `;
-      }
-    });
-}
-getVideo();
+// POP UP - MAIN - NOTFICAÇÃO
+window.addEventListener("click", (e) => {
+  const ElNotificacao = document.querySelector(".uil-bell");
+  const ElNotificacaoPopUp = document.querySelector(".notficação");
 
-// DESTAQUE
-const channelId = "UCJ1RTO5MjmLlgI5rlEEqCiA";
-const apiKey = "AIzaSyAdnNB5zv3AItIx260W4XrDfbT-PZmZKzs";
+  ElNotificacao.addEventListener("click", () => {
+    ElNotificacaoPopUp.classList.toggle("active");
+  });
 
-const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=viewCount&maxResults=3`;
+  if (e.target.tagName == "SECTION" || e.target.tagName == "HEADER") {
+    ElNotificacaoPopUp.classList.remove("active");
+  }
 
-function loadVideos() {
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      let videos = data.items;
-      let videosContainer = document.getElementById("videos");
-      videosContainer.innerHTML = "";
-
-      videos.forEach((video) => {
-        const videoId = video.id.videoId;
-        const videoTitle = video.snippet.title;
-        const videoThumbnail = video.snippet.thumbnails.medium.url;
-
-        videosContainer.innerHTML += `
-                  <div class="box video">
-                      <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
-                          <img class='hinolink img' src="${videoThumbnail}" alt="${videoTitle}">
-                      </a>
-                  </div>
-              `;
-      });
-    })
-    .catch((error) => {
-      console.error("Erro ao carregar os vídeos:", error);
-    });
-}
-
-loadVideos();
-
-//RECOMENDADOS
-const RecomendadoKey = "AIzaSyAdnNB5zv3AItIx260W4XrDfbT-PZmZKzs";
-let recomendadoUrl = `https://www.googleapis.com/youtube/v3/search?key=${RecomendadoKey}&channelId=${channelId}&part=snippet,id&order=rating&maxResults=3`;
-
-function RecomendadosFunc() {
-  fetch(recomendadoUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      let videos = data.items;
-      let videosContainer = document.getElementById("Recomandados");
-      videosContainer.innerHTML = "";
-
-      videos.forEach((video) => {
-        const videoId = video.id.videoId;
-        const videoTitle = video.snippet.title;
-        const videoThumbnail = video.snippet.thumbnails.medium.url;
-
-        videosContainer.innerHTML += `
-        <div class="box video">
-          <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
-              <img class='hinolink img' src="${videoThumbnail}" alt="${videoTitle}">
-           </a>
-        </div>
-              `;
-      });
-    })
-    .catch((error) => {
-      console.error("Erro ao carregar os vídeos:", error);
-    });
-}
-
-RecomendadosFunc();
+  if (e.target.className == "bx bx-bell") {
+    navigation.classList.remove("active");
+    menu.classList.remove("active");
+    close.classList.remove("active");
+  }
+});
 
 // SCROLL
 document.addEventListener("DOMContentLoaded", function () {
@@ -155,37 +90,8 @@ function scrollToTop() {
 
 // SEARCH
 const searchInput = document.getElementById("search-input");
-const videoContainer = document.querySelector(".pop-up-input-main");
 const mainContent = document.querySelector("#video-container");
-
-// function searchYourVideos() {
-//   const searchQuery = searchInput.value;
-//   const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyC8CaMGmTdfrBNNVYlOHWnaEWydYK8imYQ&channelId=UCJ1RTO5MjmLlgI5rlEEqCiA&q=${searchQuery}&part=snippet&type=video&maxResults=19`;
-
-//   fetch(apiUrl)
-//     .then((response) => response.json())
-//     .then((data) => {
-
-//       data.items.forEach((item) => {
-//         const videoId = item.id.videoId;
-//         const videoTitle = item.snippet.title;
-//         const videoThumbnail = item.snippet.thumbnails.medium.url;
-
-//         const videoElement = `
-//                             <div class="box-search video">
-//                                 <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
-//                                     <img class='hinolink img' src="${videoThumbnail}" alt="${videoTitle}">
-//                                 </a>
-//                             </div>
-//                         `;
-
-//         videoContainer.innerHTML += videoElement;
-//       });
-//     })
-//     .catch((error) => {
-//       console.error("Error searching for videos:", error);
-//     });
-// }
+const searchIcon = document.querySelector(".uil-search");
 
 searchInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
@@ -194,13 +100,15 @@ searchInput.addEventListener("keypress", (e) => {
 });
 
 window.addEventListener("click", (e) => {
-  if (e.target.className == "active") {
+  if (e.target.className == "pop-up-input-main active") {
     searchInput.classList.remove("active");
+    searchIcon.classList.remove("active");
     mainContent.classList.remove("active");
   }
 });
 
 searchInput.addEventListener("click", () => {
   searchInput.classList.add("active");
+  searchIcon.classList.add("active");
   mainContent.classList.add("active");
 });
